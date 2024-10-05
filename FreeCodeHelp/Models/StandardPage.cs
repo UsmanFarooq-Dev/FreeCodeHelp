f4ccaa1b385617ee636d7cb4fe7f94ee0a9ddfa9
Usman Farooq <usmanfarooq.dev@gmail.com>
2024-10-05 22:16:01 +0500

    First Check In

---
+++ FreeCodeHelp/Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace FreeCodeHelp.Models
+{
+	[PageType(Title = "Standard page")]
+	public class StandardPage : Page<StandardPage>
+	{
+	}
+}
